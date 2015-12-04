e439c11ebd3de0da08d66ae850df64567563c5d2
stevendxz <steven_dxz@sina.com>
2015-12-04 17:47:39 +0800

    create

--- src/com/adtis/fistpproj/MainActivity.java
+++ src/com/adtis/fistpproj/MainActivity.java
@@ -4,22 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
-import com.adtis.fistpproj.R;
 
 public class MainActivity extends Activity {
-    private TextView emailTextView = null;
-    private TextView pwdTextView = null;
+    private EditText emailEditText = null;
+    private EditText pwdEditText = null;
     private Button loginButton = null;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        emailTextView = (TextView)this.findViewById(R.id.login_txt_email);
-        pwdTextView = (TextView)this.findViewById(R.id.login_txt_pwd);
-        loginButton = (Button)this.findViewById(R.id.btn_login);
-        loginButton.setOnClickListener(new Button.OnClickListener(){
+        emailEditText = (EditText) this.findViewById(R.id.login_txt_email);
+        pwdEditText = (EditText) this.findViewById(R.id.login_txt_pwd);
+        loginButton = (Button) this.findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
 
