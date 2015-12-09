baa263c2052a194cff612b0112dce5a88f84d184
stevendxz <steven_dxz@sina.com>
2015-12-09 18:05:03 +0800

    create

--- src/com/adtis/fistpproj/MainActivity.java
+++ src/com/adtis/fistpproj/MainActivity.java
@@ -1,27 +1,37 @@
 package com.adtis.fistpproj;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+import com.adtis.fistpproj.adapter.ContentAdapter;
+import com.adtis.fistpproj.model.ContentModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
     final Context context = this;
     private DrawerLayout drawerLayout = null;
+    private RelativeLayout leftlayout = null;
+    private List<ContentModel> list = null;
+    private ContentAdapter adapter = null;
     private ListView listView = null;
-    private ActionBarDrawerToggle toggle = null;
-    private CharSequence sequence;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        drawerLayout = (DrawerLayout)findViewById(R.id.main);
+        leftlayout = (RelativeLayout)findViewById(R.id.main_leftlayout);
+        listView = (ListView)leftlayout.findViewById(R.id.left_drawer);
         DisplayToast("Hello");
         initView();
+        adapter = new ContentAdapter(this, list);
+        listView.setAdapter(adapter);
     }
     private NavActivity navigationView;
     private void initView() {
@@ -44,6 +54,14 @@ public class MainActivity extends Activity {
                 finish();
             }
         });
+
+        list = new ArrayList<ContentModel>();
+        list.add(new ContentModel(R.drawable.doctoradvice2, "新闻"));
+        list.add(new ContentModel(R.drawable.infusion_selected, "订阅"));
+        list.add(new ContentModel(R.drawable.mypatient_selected, "图片"));
+        list.add(new ContentModel(R.drawable.mywork_selected, "视频"));
+        list.add(new ContentModel(R.drawable.nursingcareplan2, "跟帖"));
+        list.add(new ContentModel(R.drawable.personal_selected, "投票"));
     }
 
     public void DisplayToast(String str) {
