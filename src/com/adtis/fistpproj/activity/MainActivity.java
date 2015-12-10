fc5c3651e2a7d930933f5000c8e9c305e9f76242
stevendxz <steven_dxz@sina.com>
2015-12-10 18:05:09 +0800

    mmm

--- src/com/adtis/fistpproj/activity/MainActivity.java
+++ src/com/adtis/fistpproj/activity/MainActivity.java
@@ -5,37 +5,65 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 import com.adtis.fistpproj.R;
 import com.adtis.fistpproj.adapter.ContentAdapter;
 import com.adtis.fistpproj.model.ContentModel;
+import com.nineoldandroids.view.ViewHelper;
+import com.slidingmenu.lib.SlidingMenu;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class MainActivity extends Activity {
     final Context context = this;
-    private DrawerLayout drawerLayout = null;
-    private RelativeLayout leftlayout = null;
-    private List<ContentModel> list = null;
-    private ContentAdapter adapter = null;
-    private ListView listView = null;
+    private DrawerLayout mDrawerLayout = null;
+    private NavActivity navigationView;
+    SlidingMenu menu;
+    private RelativeLayout leftLayout;
+    private List<ContentModel> list;
+    private ContentAdapter adapter;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main_activity);
-        drawerLayout = (DrawerLayout)findViewById(R.id.main);
-        leftlayout = (RelativeLayout)drawerLayout.findViewById(R.id.main_leftlayout);
-        listView = (ListView)leftlayout.findViewById(R.id.left_drawer);
-        //DisplayToast("Hello");
         initView();
-        adapter = new ContentAdapter(this, list);
+        leftLayout=(RelativeLayout) findViewById(R.id.menu_relativelayout);
+        ListView listView=(ListView) leftLayout.findViewById(R.id.left_drawer);
+        adapter=new ContentAdapter(this, list);
         listView.setAdapter(adapter);
+
+/*** 初始化侧滑菜单 Begin ***/
+        menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.layout_menu);
+/*** 初始化侧滑菜单 End ***/
     }
-    private NavActivity navigationView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
     private void initView() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main);
+        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, Gravity.RIGHT);
         navigationView = (NavActivity) super.findViewById(R.id.nav_main);
         navigationView.setTitle("主页");
         navigationView.setClickCallback(new NavActivity.ClickCallback() {
@@ -51,12 +79,13 @@ public class MainActivity extends Activity {
 
             @Override
             public void onBackClick() {
-                DisplayToast("点击了返回按钮!");
-                finish();
+                boolean flag = menu.isMenuShowing();
+                if (!flag) {
+                    menu.showMenu();
+                }
             }
         });
-
-        list = new ArrayList<ContentModel>();
+        list=new ArrayList<ContentModel>();
         list.add(new ContentModel(R.drawable.doctoradvice2, "新闻"));
         list.add(new ContentModel(R.drawable.infusion_selected, "订阅"));
         list.add(new ContentModel(R.drawable.mypatient_selected, "图片"));
@@ -64,6 +93,7 @@ public class MainActivity extends Activity {
         list.add(new ContentModel(R.drawable.nursingcareplan2, "跟帖"));
         list.add(new ContentModel(R.drawable.personal_selected, "投票"));
     }
+
 
     public void DisplayToast(String str) {
         Toast.makeText(this, str, Toast.LENGTH_LONG).show();
