deaa8fcb34a367ea04597f4754649f952f079a90
stevendxz <steven_dxz@sina.com>
2015-12-17 17:58:36 +0800

    20151216

--- src/com/adtis/fistpproj/activity/MainActivity.java
+++ src/com/adtis/fistpproj/activity/MainActivity.java
@@ -1,15 +1,29 @@
 package com.adtis.fistpproj.activity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.Toast;
 import com.adtis.fistpproj.R;
+import com.adtis.fistpproj.model.ADInfo;
 import com.adtis.fistpproj.util.CustomToast;
+import com.adtis.fistpproj.util.CycleViewPager;
+import com.adtis.fistpproj.util.ViewFactory;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.slidingmenu.lib.SlidingMenu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
     final Context context = this;
@@ -17,12 +31,26 @@ public class MainActivity extends Activity {
     private NavActivity navigationView;
     private SlidingMenu menu;
 
+    private List<ImageView> views = new ArrayList<ImageView>();
+    private List<ADInfo> infos = new ArrayList<ADInfo>();
+    private CycleViewPager cycleViewPager;
+
+    private boolean ismove = false;
+
+    private String[] imageUrls = {"http://img.taodiantong.cn/v55183/infoimg/2013-07/130720115322ky.jpg",
+            "http://pic30.nipic.com/20130626/8174275_085522448172_2.jpg",
+            "http://pic18.nipic.com/20111215/577405_080531548148_2.jpg",
+            "http://pic15.nipic.com/20110722/2912365_092519919000_2.jpg",
+            "http://pic.58pic.com/58pic/12/64/27/55U58PICrdX.jpg"};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main_activity);
         initView();
+        configImageLoader();
+        initialize();
     }
 
     private void initView() {
@@ -33,7 +61,7 @@ public class MainActivity extends Activity {
 
             @Override
             public void onRightClick() {
-                CustomToast.DisplayToast(context,"点击了设置按钮");
+                CustomToast.DisplayToast(context, "点击了设置按钮");
             }
 
             @Override
@@ -56,5 +84,99 @@ public class MainActivity extends Activity {
         menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
         menu.setMenu(R.layout.menu_activity);
         /*** 初始化侧滑菜单 End ***/
+    }
+
+    @SuppressLint("NewApi")
+    private void initialize() {
+
+        cycleViewPager = (CycleViewPager) getFragmentManager().findFragmentById(R.id.fragment_cycle_viewpager_content);
+
+        for (int i = 0; i < imageUrls.length; i++) {
+            ADInfo info = new ADInfo();
+            info.setUrl(imageUrls[i]);
+            info.setContent("图片-->" + i);
+            infos.add(info);
+        }
+
+        // 将最后一个ImageView添加进来
+        views.add(ViewFactory.getImageView(this, infos.get(infos.size() - 1).getUrl()));
+        for (int i = 0; i < infos.size(); i++) {
+            views.add(ViewFactory.getImageView(this, infos.get(i).getUrl()));
+        }
+        // 将第一个ImageView添加进来
+        views.add(ViewFactory.getImageView(this, infos.get(0).getUrl()));
+
+        // 设置循环，在调用setData方法前调用
+        cycleViewPager.setCycle(true);
+
+        // 在加载数据前设置是否循环
+        cycleViewPager.setData(views, infos, mAdCycleViewListener);
+        //设置轮播
+        cycleViewPager.setWheel(true);
+
+        // 设置轮播时间，默认5000ms
+        cycleViewPager.setTime(2000);
+        //设置圆点指示图标组居中显示，默认靠右
+        cycleViewPager.setIndicatorCenter();
+
+    }
+
+    private CycleViewPager.ImageCycleViewListener mAdCycleViewListener = new CycleViewPager.ImageCycleViewListener() {
+
+        @Override
+        public void onImageClick(ADInfo info, int position, View imageView) {
+            if (cycleViewPager.isCycle()) {
+                position = position - 1;
+                Toast.makeText(MainActivity.this,
+                        "position-->" + info.getContent(), Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    };
+
+    private CycleViewPager.ImageCycleViewTouchListener adCycleViewTouchListener = new CycleViewPager.ImageCycleViewTouchListener() {
+        @Override
+        public boolean isCanMove(View view) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            ismove = true;
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+                            ismove = true;
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            ismove = false;
+                            break;
+                        default:
+                            break;
+                    }
+                    return false;
+                }
+            });
+            return false;
+        }
+    };
+
+    /**
+     * 配置ImageLoder
+     */
+    private void configImageLoader() {
+        // 初始化ImageLoader
+        @SuppressWarnings("deprecation")
+        DisplayImageOptions options = new DisplayImageOptions.Builder().showStubImage(R.drawable.icon_stub) // 设置图片下载期间显示的图片
+                .showImageForEmptyUri(R.drawable.icon_empty) // 设置图片Uri为空或是错误的时候显示的图片
+                .showImageOnFail(R.drawable.icon_error) // 设置图片加载或解码过程中发生错误显示的图片
+                .cacheInMemory(true) // 设置下载的图片是否缓存在内存中
+                .cacheOnDisc(true) // 设置下载的图片是否缓存在SD卡中
+                // .displayer(new RoundedBitmapDisplayer(20)) // 设置成圆角图片
+                .build(); // 创建配置过得DisplayImageOption对象
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext()).defaultDisplayImageOptions(options)
+                .threadPriority(Thread.NORM_PRIORITY - 2).denyCacheImageMultipleSizesInMemory()
+                .discCacheFileNameGenerator(new Md5FileNameGenerator()).tasksProcessingOrder(QueueProcessingType.LIFO).build();
+        ImageLoader.getInstance().init(config);
     }
 }
